ab5dbe7f90e52a603a0e076601e939ae529e1210
nishan8583 <nishanmaharjan17@gmail.com>
2020-06-15 09:23:09 +0545

    First Commmit

---
+++ src/Component/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div style = {{display:'flex',justifyContent:'flex-end'}}>
+            <nav className='f3 link dim underline pa3 pointer'>
+                <p>Sign Out</p>
+            </nav>
+        </div>
+    );
+};
+
+export default Navigation
